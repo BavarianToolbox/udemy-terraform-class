0a1b4565e9e0b730eca3c9f560a34dd21eaa137a
Constantin Baumgartner <constantin.baumgartner@gmail.com>
2021-08-23 13:30:02 -0400

    add provisioner examples

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ variable "env_prefix" {}
 variable "my_ip" {}
 variable "instance_type"{}
 variable "public_key_location" {}
+variable "private_key_location" {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -92,11 +93,11 @@ data "aws_ami" "latest-amazon-linux-image" {
     }
 }
 
-output "aws-ami-id" {
+output "aws_ami_id" {
     value = data.aws_ami.latest-amazon-linux-image.id
 }
 
-output "ect_public_ip" {
+output "ec2_public_ip" {
     value = aws_instance.myapp-server.public_ip
 }
 
@@ -116,11 +117,25 @@ resource "aws_instance" "myapp-server" {
     associate_public_ip_address = true
     key_name = aws_key_pair.ssh-key.key_name
 
-    user_data = file("entry-script.sh")
+    # user_data = file("entry-script.sh")
+
+    connection {
+        type = "ssh"
+        host = self.public_ip
+        user = "ec2-user"
+        agent = true
+    }
+
+    provisioner "file" {
+        source = "entry-script.sh"
+        destination = "/home/ect-user/entry-script.sh"
+    }
+
+    provisioner "remote-exec" {
+        script = file("entry-script.sh") 
+    }
 
     tags = {
         Name = "${var.env_prefix}-server"
     }
 }
-
-# sohini github: sohiniroych
