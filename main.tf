1fd99dc354058eaaf0ca01e62858f8235dc5e69e
Constantin Baumgartner <constantin.baumgartner@gmail.com>
2021-08-23 17:46:02 -0400

    switched to s3 backend

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+    required_version = ">= 0.12"
+    backend "s3" {
+        bucket = "constantin-terraform-state-bucket"
+        key = "myapp/state.tfstate"
+        region = "us-east-2"
+    }
+}
+
 provider "aws" {
     region = "us-east-2" 
 }
